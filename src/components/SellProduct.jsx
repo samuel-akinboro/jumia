b6f5bbcea833d06493f594204f976189eff44756
samuel <victorakinboro2003@gmail.com>
2021-03-01 22:56:48 +0100

    first commit change

---
+++ src/components/SellProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SellProduct.css'
+
+function SellProduct() {
+  return (
+    <div className="sell__product">
+      hello
+    </div>
+  )
+}
+
+export default SellProduct
